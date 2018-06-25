a8db8b17e36ba7131c14fb62f8c67436624dc8c9
mafinar <mafinar@gmail.com>
2018-06-24 22:55:40 -0400

    Add some height helper functions and skeletons for rotations

--- src/double_booked/avl.clj
+++ src/double_booked/avl.clj
@@ -11,7 +11,7 @@
   {:pre [(< start finish)]}
   "Makes a Node from strings id, start and finish"
   {:start  start
-   :finish   finish
+   :finish finish
    :id     id
    :max    -1
    :height -1
@@ -19,6 +19,31 @@
    :right  nil})
 
 ;; --------------------------------------- HEIGHT/MAX HELPERS
+
+(defn get-max [node]
+  "Returns the max key of a node, 0 if nil"
+  (get node :max 0))
+
+(defn get-height [node]
+  "Returns the :height key of the node, 0 if nil"
+  (get node :height 0))
+
+(defn get-max-height [{left :left right :right}]
+  "Returns the max height between the left and right subtrees"
+  (max (get-height left) (get-height right)))
+
+(defn get-height-diff [node]
+  "Returns the height difference of the node."
+  (match [node]
+         [nil] 0
+         [{:left left :right right}]
+         (- (get-height left) (get-height right))))
+
+(defn refresh-max [{:keys [finish left right] :as node}]
+  (let [max (max (get-max left)
+                 (get-max right)
+                 finish)]
+    (assoc node :max max)))
 
 ;; --------------------------------------- BALANCERS
 (defn left-rotate [node] :left-rotate)
