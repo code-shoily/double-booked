731edad21ece0b9c0dcfab87ea8ec1f68986ffec
mafinar <mafinar@gmail.com>
2018-06-24 22:53:51 -0400

    Clean up unused deps and update license key

--- project.clj
+++ project.clj
@@ -1,14 +1,11 @@
 (defproject double_booked "0.5.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins  [[cider/cider-nrepl "0.15.1"]]
+  :license {:name "The MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-time "0.14.4"]
-                 [org.clojure/core.match "0.3.0-alpha5"]
-                 [org.clojure/tools.nrepl "0.2.12"]
-                 [cljfmt "0.5.7"]]
+                 [org.clojure/core.match "0.3.0-alpha5"]]
   :main ^:skip-aot double-booked.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
